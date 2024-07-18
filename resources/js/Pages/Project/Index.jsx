dc8cf22b61e737f3afe977417b5ff1e78a7fdbc0
Bp3g3H <zaimpuls1@gmail.com>
2024-07-18 18:18:47 +0300

    1. Adding create and store action for Project 2. Adding the rules for project create 3. Handling image_path to be from storage 4. Adding fillables to Project 5. Creating new component for TextAreaInput 6. Creating new page for Create which holds the form for Project 7. Adding the add new button and success message

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -7,7 +7,7 @@ import Authenticated from "@/Layouts/AuthenticatedLayout";
 import { Head, Link, router } from '@inertiajs/react';
 
 
-export default function Index({ auth, projects, queryParams = null }) {
+export default function Index({ auth, projects, queryParams = null, success = null }) {
     queryParams = queryParams || {};
     const searchFieldChanged = (name, value) => {
         if (value) {
@@ -39,12 +39,19 @@ export default function Index({ auth, projects, queryParams = null }) {
     return (
         <Authenticated
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Projects</h2>}
+            header={
+                <div className="flex justify-between items-center">
+                    <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Projects</h2>
+                    <Link href={route('project.create')} className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600" >Add Project</Link>
+                </div>
+            }
         >
             <Head title="Projects" />
-
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    {success && (<div className="bg-emerald-500 py-2 px-4 text-white mb-4 rounded">
+                        {success}
+                    </div>)}
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
                             <div className="overflow-auto">
